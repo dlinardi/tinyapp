036cf09f3ba9f7445c94331a03caf093459e9aae
dlinardi <dlinardi98@gmail.com>
2020-11-10 19:00:18 +0000

    implemented POST request for deleting url entry

--- express_server.js
+++ express_server.js
@@ -41,14 +41,18 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  
+
   if (urlDatabase[req.params.shortURL]) {
     res.render("urls_show", templateVars);
   } else {
-    res.statusCode = 404;
-    res.send(`${res.statusCode} That URL doesn't exit.`);
+    res.send("That URL doesn't exit.");
   }
 });
 
@@ -56,7 +60,7 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
-
+  
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
